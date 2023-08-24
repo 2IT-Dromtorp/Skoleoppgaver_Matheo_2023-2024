d7ca56584b5f0897d2fb12e94e0651500de5c725
Matheo Pangopoulos <matheokantpangopoulos@gmail.com>
2023-08-24 08:53:48 +0200

    Added .gitignore

---
+++ react_intro/react_intro_app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://scontent.fosl1-1.fna.fbcdn.net/v/t1.6435-9/36944531_116985529219962_345109083319173120_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=ehDCJez7hRMAX-zbMVa&_nc_ht=scontent.fosl1-1.fna&oh=00_AfBdx7unY8ur-_ECaY1Qi8bPqTUtm0BpEVsqXspnFI4MKA&oe=650D5803" className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
