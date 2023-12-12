151cf979d8440256669cec0af3cb9bf865df4fdd
Matheo Pangopoulos <matheokantpangopoulos@gmail.com>
2023-12-12 15:43:39 +0100

    Continued

--- proveeksamen2023/proveeksamen2023/src/pageSetUp/pages/createTicket.js
+++ proveeksamen2023/proveeksamen2023/src/pageSetUp/pages/createTicket.js
@@ -1,13 +1,45 @@
 import '../../css/createticket.css'
 
 import { useContext, useEffect, useState } from 'react';
-
+import { Link } from 'react-router-dom';
 
 function CreateTicket() {
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("NEINIE")
+    }
+
     return (
         <div className='createticket-main'>
-            <p>opfewølerli</p>
+            <div className='createticket-ad-container'>
+                <Link to={"/"}>--</Link>
+            </div>
+            <div className='createticket-create-container'>
+                <form className='ticker-creator' onSubmit={handleSubmit}>
+                    <label className='createticket-labels'>
+                        Brukerens navn
+                        <input type="text" required={true} autoComplete='username'/>
+                    </label>
+                    <label className='createticket-labels'>
+                        Kort beskrivelse av problemet
+                        <input type="text" required={true} autoComplete='off'/>
+                    </label>
+                    <label className='createticket-labels'>
+                        En mer detaljert beskrivelse av problemet
+                        <textarea required={true} autoComplete='off'/>
+                    </label>
+                    <label className='createticket-labels'>
+                        Hvor alvorlig er problemet?
+                        <select name="prioritiez">
+                            <option value="volvo">Høyst kritisk</option>
+                            <option value="saab">Moderat</option>
+                            <option value="fiat">Lav</option>
+                        </select>
+                    </label>
+                    <input type="submit" value={"Send inn ticket"}/>
+                </form>
+            </div>
         </div>
     );
 }
--- proveeksamen2023/proveeksamen2023/src/pageSetUp/pages/homesite.js
+++ proveeksamen2023/proveeksamen2023/src/pageSetUp/pages/homesite.js
@@ -12,7 +12,11 @@ function Homesite() {
             </div>
             <div className='homesite-create-ticket-main'>
                 <div className='homesite-display-ticket-settings'>
-
+                    <p className='homesite-column'>Sak Nr</p>
+                    <p className='homesite-column'>Beskrivelse</p>
+                    <p className='homesite-column'>Bruker</p>
+                    <p className='homesite-column'>Status</p>
+                    <p className='homesite-column'>Prioritet</p>
                 </div>
                 <div className='homesite-create-ticket-container'>
 
