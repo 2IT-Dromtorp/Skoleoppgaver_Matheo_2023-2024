9c2cf91097390389327b257c8636a7a6be4b0aa5
Matheo Pangopoulos <matheokantpangopoulos@gmail.com>
2024-04-23 13:11:22 +0200

    small change

--- security-mongodb/frontend/src/structure/pages/createuser/createUser.jsx
+++ security-mongodb/frontend/src/structure/pages/createuser/createUser.jsx
@@ -116,11 +116,11 @@ export default function CreateUser() {
         {showDialog?<DialogBox DataFromUser={DataForFamilyDialog}/>:""}
 
         <div className="login-main">
-            <div className="login-info">
+            {/* <div className="login-info">
 
-            </div>
+            </div> */}
             <div className="login-login">
-
+                <p className='login-log-in-header'>Create a new user</p>
                 <form onSubmit={handleLogin} className="login-form">
                     <select required={true} onChange={(e)=>setSchoolclassLoc(e.target.value)} value={schoolclass} placeholder="Your class" className='createuser-input-field createuser-select-field'>
                         <option value="None">Select a class</option>
@@ -131,16 +131,16 @@ export default function CreateUser() {
                         <option value="LAERER">Lærer</option>
                     </select>
 
-                    <input className="createuser-input-field" required={true} type="text" onChange={(e)=>setGivenName(e.target.value)} value={givenName} placeholder="Firstname"/>
-                    <input className="createuser-input-field" required={true} type="text" onChange={(e)=>setSurname(e.target.value)} value={surname} placeholder="Lastname"/>
-                    <input className="createuser-input-field" required={true} autoComplete="email" type="email" onChange={(e)=>setEmailLoc(e.target.value)} value={email} placeholder="Email"/>
-                    <input className="createuser-input-field" required={true} autoComplete="password" type="password" onChange={(e)=>setPassword(e.target.value)} value={password} placeholder="Password" minLength={8}/>
-                    <input className="createuser-input-field" required={true} autoComplete="password" type="password" onChange={(e)=>setPasswordCheck(e.target.value)} value={passwordCheck} placeholder="Repeat your password" minLength={8}/>
+                    <input className="createuser-input-field" required={true} type="text" onChange={(e)=>setGivenName(e.target.value)} value={givenName} placeholder="Firstname*"/>
+                    <input className="createuser-input-field" required={true} type="text" onChange={(e)=>setSurname(e.target.value)} value={surname} placeholder="Lastname*"/>
+                    <input className="createuser-input-field" required={true} autoComplete="email" type="email" onChange={(e)=>setEmailLoc(e.target.value)} value={email} placeholder="Email*"/>
+                    <input className="createuser-input-field" required={true} autoComplete="password" type="password" onChange={(e)=>setPassword(e.target.value)} value={password} placeholder="Password*" minLength={8}/>
+                    <input className="createuser-input-field" required={true} autoComplete="password" type="password" onChange={(e)=>setPasswordCheck(e.target.value)} value={passwordCheck} placeholder="Repeat your password*" minLength={8}/>
                     
-                    <input className="createuser-input-field" required={schoolclass!=="LAERER"} type="tel" pattern="[0-9]{8}" placeholder='Phone number' value={phone} onChange={(e)=>setPhone(e.target.value)}/>
-                    <input className="createuser-input-field" required={schoolclass!=="LAERER"} autoComplete="address" type="text" onChange={(e)=>setAddress(e.target.value)} value={address} placeholder="Address"/>
+                    <input className="createuser-input-field" required={schoolclass!=="LAERER"} type="tel" pattern="[0-9]{8}" value={phone} onChange={(e)=>setPhone(e.target.value)} placeholder={`Phone number${schoolclass!=="LAERER"?"*":""}`}/>
+                    <input className="createuser-input-field" required={schoolclass!=="LAERER"} autoComplete="address" type="text" onChange={(e)=>setAddress(e.target.value)} value={address} placeholder={`Address${schoolclass!=="LAERER"?"*":""}`}/>
                     
-                    {schoolclass!=="LAERER"?<button type="button" onClick={()=>setShowDialog(true)} className='createuser-input-add-family-button'>Add a family member</button>:""}
+                    {schoolclass!=="LAERER"?<button type="button" onClick={()=>setShowDialog(true)} className='createuser-input-add-family-button'>Add a family member*</button>:""}
 
                     {dialogContent.operation==="addFamilyMember"&&dialogContent.data.length?dialogContent.data.map((member, index) =>
                         <KinComponent key={index} index={index} name={member.name} address={member.address} phonenumber={member.phone} email={member.email} use="createUser"/>
--- security-mongodb/frontend/src/structure/pages/loginpage/logInPage.jsx
+++ security-mongodb/frontend/src/structure/pages/loginpage/logInPage.jsx
@@ -54,13 +54,15 @@ export default function LogInPage() {
 
     return (
         <div className="login-main">
-            <div className="login-info">
+            {/* <div className="login-info">
 
-            </div>
+            </div> */}
             <div className="login-login">
+                 <p className="login-log-in-header">Log in</p>
+
                 <form onSubmit={handleLogin} className="login-form">
-                    <input className="login-input-field" required={true} autoComplete="email" type="email" onChange={(e)=>setemailloc(e.target.value)} value={emailloc} placeholder="Email"/>
-                    <input className="login-input-field" required={true} autoComplete="password" type="password" onChange={(e)=>setPassword(e.target.value)} value={password} placeholder="Password"/>
+                    <input className="login-input-field" required={true} autoComplete="email" type="email" onChange={(e)=>setemailloc(e.target.value)} value={emailloc} placeholder="Email*"/>
+                    <input className="login-input-field" required={true} autoComplete="password" type="password" onChange={(e)=>setPassword(e.target.value)} value={password} placeholder="Password*"/>
                     <button type="submit" className="login-login-button">Log in</button>
                 </form>
                 <Link to="/create-user" className="login-create-user-link">Create a new user</Link>
